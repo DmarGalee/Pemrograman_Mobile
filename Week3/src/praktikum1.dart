616dbe4f647acec930844ebf3ffefe1fa158183a
Damar Galih Fitrianto <damargalih1502@gmail.com>
2025-09-08 17:56:48 +0700

    Week3_Praktikum1_selesai

---
+++ Week3/src/praktikum1.dart
@@ -0,0 +1,18 @@
+void main() {
+  String test = "test2";
+  bool isTrue = true;
+
+  if (test == "test1") {
+    print("Test1");
+  } else if (test == "test2") {
+    print("Test2");
+  } else {
+    print("Something else");
+  }
+
+  if (test == "test2") print("Test2 again");
+
+  if (isTrue) {
+    print("Kebenaran");
+  }
+}
